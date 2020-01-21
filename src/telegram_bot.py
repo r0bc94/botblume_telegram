74b87e9bdc5d21e7a0084e153829fef7c7c9656b
Robin <robin.pal@hotmail.de>
2020-01-21 21:42:38 +0100

    :sparkles: Add the Callback to the Telegram Bot

--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -2,19 +2,17 @@ import logging
 from telegram.ext import Updater, CommandHandler
 
 class TelegramBot():
-  def __init__(self, token):
+  def __init__(self, token, messageCallback):
     self.__updater = Updater(token=token, use_context=True)
     self.__dispatcher = self.__updater.dispatcher
     self.__logger = logging.getLogger().getChild('TelegramBot')
-
+    self.__messageCallback = messageCallback
 
   def setupAndStart(self):
-    testHandler = CommandHandler('test', self.test)
-    self.__dispatcher.add_handler(testHandler)
+    self.__dispatcher.add_handler(CommandHandler('wasserstand', self.__flowerCommand))
 
-  def test(self, update, context):
-    print('lol')
-    context.bot.send_message(chat_id=update.effective_chat.id, text='lol')
+  def __flowerCommand(self, update, context):
+    self.__messageCallback(update, context)
     
   def listen(self):
     self.__updater.start_polling()
